adcd298592b4c4e4f231749224569c5845b48403
Yasin <yasinarabi1383@gmail.com>
2025-05-12 20:14:02 +0330

    Add WeatherModel to encapsulate weather data, including temperature, humidity, wind speed, air pollution, and location details

---
+++ ResponseModel/WeatherModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherAPI.ResponseModel
+{
+    public class WeatherModel
+    {
+        public double Temp { get; set; }
+        public double FeelsTemp { get; set; }
+        public int Humidity { get; set; }
+        public double WindSpeed { get; set; }
+        public AirPollutionModel? AirPollutionInfo { get; set; }
+        public CityModel? LocationInfo { get; set; }
+    }
+}
