389713b2be067a38f87d2ad29f842a934103a8ff
Yasin <yasinarabi1383@gmail.com>
2025-05-12 22:09:11 +0330

    Define IOpenWeatherServices interface to standardize methods for fetching city, weather, and air pollution data

---
+++ Interfaces/IOpenWeatherServices.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WeatherAPI.ResponseModel;
+
+namespace WeatherAPI.Interfaces
+{
+    public interface IOpenWeatherServices
+    {
+        Task<CityModel> GetCityInfo(string cityName);
+        Task<WeatherModel> GetWeatherInfo(double latitude, double longitude);
+        Task<AirPollutionModel> GetAirPollutionInfo(double latitude, double longitude);
+    }
+}
