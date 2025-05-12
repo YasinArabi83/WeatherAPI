227fe5b0f0b9933729aa26686287333db7d59503
Yasin <yasinarabi1383@gmail.com>
2025-05-12 19:42:20 +0330

    "Add CityModel to represent city details including location and localized name"

---
+++ ResponseModel/CityModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherAPI.ResponseModel
+{
+    public class CityModel
+    {
+        public string? Country { get; set; }
+        public string? Province { get; set; }
+        public string? Name { get; set; }
+        public string? NameInFarsi { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+
+    }
+}
