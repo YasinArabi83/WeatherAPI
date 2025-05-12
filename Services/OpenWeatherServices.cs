7b2f8c3f78bd93ae9c3c68d7fecd1246cee03377
Yasin <yasinarabi1383@gmail.com>
2025-05-13 00:25:31 +0330

    Created `OpenWeatherServices` class implementing `IOpenWeatherServices` with methods for fetching weather-related data (implementations pending).

---
+++ Services/OpenWeatherServices.cs
@@ -0,0 +1,26 @@
+﻿using WeatherAPI.Interfaces;
+using WeatherAPI.ResponseModel;
+
+namespace WeatherAPI.Services;
+
+public class OpenWeatherServices(ApiFetcher fetch, ILogger<OpenWeatherServices> logger, HttpClient client) : IOpenWeatherServices
+{
+    private readonly ApiFetcher _fetch = fetch;
+    private readonly ILogger<OpenWeatherServices> _logger = logger;
+    private readonly HttpClient _client=client;
+
+    public Task<AirPollutionModel> GetAirPollutionInfo(double latitude, double longitude)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CityModel> GetCityInfo(string cityName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<WeatherModel> GetWeatherInfo(double latitude, double longitude)
+    {
+        throw new NotImplementedException();
+    }
+}
