810f9c4d616eb711dbb80b7af2ee2895271012aa
Yasin <yasinarabi1383@gmail.com>
2025-05-13 19:35:18 +0330

    Enhance OpenWeatherServices with async methods
    - Updated `GetAirPollutionInfo` to be asynchronous,
      fetching air pollution data with improved error handling
      and logging.
    
    - Refined error message in `GetCityInfo` for clarity
      when city information is not found.

--- Services/OpenWeatherServices.cs
+++ Services/OpenWeatherServices.cs
@@ -13,9 +13,44 @@ public class OpenWeatherServices
     private readonly HttpClient _client = client;
     private readonly IConfiguration _configuration = configuration;
 
-    public Task<AirPollutionModel> GetAirPollutionInfo(double latitude, double longitude)
+    public async Task<AirPollutionModel> GetAirPollutionInfo(double latitude, double longitude)
     {
-        throw new NotImplementedException();
+        string endPoint = "data/2.5/air_pollution";
+        try
+        {
+            var queryParams = new Dictionary<string, string?>
+            {
+                ["lat"] = latitude.ToString(),
+                ["lon"] = longitude.ToString(),
+                ["appid"] = _configuration["Token"] ?? throw new NullReferenceException("API token is missing in the configuration. Please ensure the 'Token' key is set in the tokens.json file."),
+            };
+
+            var urlWithQuery = QueryHelpers.AddQueryString(endPoint, queryParams);
+
+            _logger.LogInformation("Fetching air pollution information for {lat} & {lon} from endpoint {urlWithQuery}", latitude , longitude, urlWithQuery);
+
+            var response = await _fetch.GetApis(_client, urlWithQuery);
+
+            var airPollut = AirPollutionModel.ParseAirPollution(response);
+
+            _logger.LogInformation("Successfully retrieved air pollution information for {lat} & {lon}", latitude, longitude);
+            return airPollut ?? throw new Exception($"air pollution information for {latitude} & {longitude} is not found");
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex, "HTTP request error while fetching air pollution information for {lat} & {lon}", latitude, longitude);
+            throw new Exception("An error occurred while making the HTTP request.", ex);
+        }
+        catch (NullReferenceException ex)
+        {
+            _logger.LogError(ex, "Configuration error: {Message}", ex.Message);
+            throw;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred while fetching air pollution information for {lat} & {lon}", latitude, longitude);
+            throw;
+        }
     }
 
     public async Task<CityModel> GetCityInfo(string cityName)
@@ -38,7 +73,7 @@ public class OpenWeatherServices
             var cityInfo = CityModel.ParseCities(response).FirstOrDefault();
 
             _logger.LogInformation("Successfully retrieved city information for {CityName}", cityName);
-            return cityInfo??throw new Exception("City information is null");
+            return cityInfo??throw new Exception($"City {cityName} is not found");
         }
         catch (HttpRequestException ex)
         {
