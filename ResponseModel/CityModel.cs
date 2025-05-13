89f6de8fe6601e8caba06b9c0529c944c0bfc88e
Yasin <yasinarabi1383@gmail.com>
2025-05-13 18:22:42 +0330

    Add ParseCities method to CityModel for JSON parsing
    The CityModel class in the WeatherAPI.ResponseModel namespace now includes a static method called ParseCities. This method accepts a JSON string, parses it into a JArray, and creates a list of CityModel instances. Each instance is populated with data such as country, province, name, name in Farsi, latitude, and longitude, and the method returns the constructed list of cities.

--- ResponseModel/CityModel.cs
+++ ResponseModel/CityModel.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherAPI.ResponseModel
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherAPI.ResponseModel
 {
     public class CityModel
     {
@@ -9,6 +11,27 @@
         public double Longitude { get; set; }
         public double Latitude { get; set; }
 
+        public static List<CityModel> ParseCities(string json)
+        {
+            var jArray = JArray.Parse(json);
+            var cities = new List<CityModel>();
 
+            foreach (var item in jArray)
+            {
+                var city = new CityModel
+                {
+                    Country = item["country"]?.ToString(),
+                    Province = item["state"]?.ToString(),
+                    Name = item["name"]?.ToString(),
+                    NameInFarsi = item["local_names"]?["fa"]?.ToString(),
+                    Latitude = item["lat"]?.ToObject<double>() ?? 0,
+                    Longitude = item["lon"]?.ToObject<double>() ?? 0
+                };
+
+                cities.Add(city);
+            }
+
+            return cities;
+        }
     }
 }
