5991be602e42515e01f89baaa3c661c86eac1041
Yasin <yasinarabi1383@gmail.com>
2025-05-13 20:49:54 +0330

    Fix namespace and add JSON parsing to WeatherModel
    Corrected the namespace declaration and added the
    `using Newtonsoft.Json.Linq;` directive for JSON parsing.
    Introduced a static method `ParseWeather` to parse a
    JSON string and populate the `WeatherModel` properties
    with relevant data, initializing `LocationInfo` and
    `AirPollutionInfo` to `null`.

--- ResponseModel/WeatherModel.cs
+++ ResponseModel/WeatherModel.cs
@@ -1,4 +1,6 @@
-﻿namespace WeatherAPI.ResponseModel
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherAPI.ResponseModel
 {
     public class WeatherModel
     {
@@ -8,5 +10,22 @@
         public double WindSpeed { get; set; }
         public AirPollutionModel? AirPollutionInfo { get; set; }
         public CityModel? LocationInfo { get; set; }
+
+        public static WeatherModel ParseWeather(string json)
+        {
+            var root = JObject.Parse(json);
+
+            var weather = new WeatherModel
+            {
+                Temp = root["main"]?["temp"]?.ToObject<double>() ?? 0,
+                FeelsTemp = root["main"]?["feels_like"]?.ToObject<double>() ?? 0,
+                Humidity = root["main"]?["humidity"]?.ToObject<int>() ?? 0,
+                WindSpeed = root["wind"]?["speed"]?.ToObject<double>() ?? 0,
+                LocationInfo = null,
+                AirPollutionInfo = null
+            };
+
+            return weather;
+        }
     }
 }
